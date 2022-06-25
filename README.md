9caeeb931322b8b7fef9f05875693bc370c82af6
duhongwei <duhongwei@360.cn>
2022-06-25 10:26:20 +0800

    init

---
+++ README.md
@@ -0,0 +1,47 @@
+RouterDelegate17 是 RouterDelegate 的子类。用 Navgator2.0 实现的了 push,pop,replace 的方法。与 Navgator1.0不同的是，可以方便的跳转到页面栈中的任意页面。 
+
+## Features
+1. `Future<T?> push<T>(Page<T> page)` 页面入栈。如果待入栈的页面已经存在，上面的页面全部弹出。也就是说可以跳转到栈中任意页面。
+2. `bool pop<T extends Object>` 栈顶的页面出栈。
+3. `Future<T?> replace<T, TO>(Page<T> page, {TO? result})` 页面替换。如果新页面在栈中已经存在，则栈顶的页面全部弹出。
+4. 页面状态监控。比如 A,B，二个页面。栈中有 A 页面， B 入栈，A 页面响应 `PageStatus.leave`，因为此时 A 页面被 B 页面覆盖了。B 页面 弹出 A 页面响应 `PageStatus.enter` 方法，因为 此时 A 页面又重新显示了。如果页面是首次入栈，页面的状态是 `PageStatus.none`
+5. 退出程序监控。每当要退出首页（比如在首页按物理退出键）都会记录下来，并报告次数。
+一般2秒（默认）内连按两次就需要退出程序。按一次可以 toast 通知。
+6. openDialog。 对 showDialog 的包装，为了让 dialog下面的页面可以及时修改`PageStatus`，如果直接调用 showDialog，dialog下面的页面不会改变状态。
+7. openModalBottomSheet 对 showModalBottomSheet 的包装，为了让 ModalBottomSheet 下面的页面可以及时修改`PageStatus`。如果直接调用  showModalBottomSheet，ModalBottomSheet 下面的页面不会改变状态。
+## Usage
+
+1. 安装
+`flutte pub get router_delegate17`
+2. 为了方便说明，生成一个全局变量 `routerDelegate = RouterDelegate17()`，一个页面 PageA,一个页面PageB，一个页面 C
+在 main.dart 中，初始化首页，引用 routerDelegate
+```dart
+void main() async {
+  await app.init(pages: [MaterialPage(child: PageA())]);
+  runApp(const MyApp());
+}
+Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter template',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Router(
+        routerDelegate: routerDelegate,
+        backButtonDispatcher: RootBackButtonDispatcher(),
+      ),
+    );
+  }
+然后就可以使用 `routerDelegate.push` 等方法进行页面跳转了。
+```
+3. 页面 A 中 PageA 跳转 PageB `routerDelegate.push(PageB())`,在页面B中 PageB跳PageC `routerDelegate.push(PageC())`
+4. 在页面 C 中可以直接回到 页面A `routerDelegate.push(PageA())`
+5. 在跳转的时候页面上会显示页面当前的状态。
+
+其它的演示效果直接看完整示例吧，在这里。
+
+## Additional information
+
+RouterDelegate17 是 RouterDelegate 的子类。就是帮你实现的必要的方法，方便使用。
+
+为了简化逻辑，方便使用，RouterDelegate17 暂时并不支持 web 开发。
---
+++ lib/router_delegate17.dart
@@ -0,0 +1,222 @@
+library router_delegate17;
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class RouterDelegate17 extends RouterDelegate<RouteSettings>
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin<RouteSettings> {
+  //RouterDelegate17._();
+  //static final RouterDelegate17 _router = RouterDelegate17._();
+
+  /* factory RouterDelegate17() {
+    return _router;
+  } */
+
+  final Duration exitDelay;
+  final ExitCount exitCount;
+  RouterDelegate17({this.exitDelay = const Duration(seconds: 2)}):exitCount=ExitCount(delay:exitDelay );
+
+  NavSettings get currentNavSettings => _settingsList.last;
+  NavSettings? get previousNavSettings {
+    var len = _settingsList.length;
+    if (len < 2) return null;
+    return _settingsList[len - 2];
+  }
+
+  Future<T?> openDialog<T>({
+    required BuildContext context,
+    required WidgetBuilder builder,
+    bool barrierDismissible = true,
+    Color? barrierColor = Colors.black54,
+    String? barrierLabel,
+    bool useSafeArea = true,
+    bool useRootNavigator = true,
+    RouteSettings? routeSettings,
+  }) async {
+    currentNavSettings.status.value = PageStatus.leave;
+    var result = await showDialog<T>(
+        context: context,
+        builder: builder,
+        barrierDismissible: barrierDismissible,
+        barrierColor: barrierColor,
+        barrierLabel: barrierLabel,
+        useSafeArea: useSafeArea,
+        useRootNavigator: useRootNavigator,
+        routeSettings: routeSettings);
+    currentNavSettings.status.value = PageStatus.enter;
+    return result;
+  }
+
+  void closeModalRoute<T extends Object?>([T? result]) {
+    navigatorKey?.currentState?.pop<T>(result);
+  }
+
+  Future<T?> openModalBottomSheet<T>({
+    required BuildContext context,
+    required WidgetBuilder builder,
+    Color? backgroundColor,
+    double? elevation,
+    ShapeBorder? shape,
+    Clip? clipBehavior,
+    BoxConstraints? constraints,
+    Color? barrierColor,
+    bool isScrollControlled = false,
+    bool useRootNavigator = false,
+    bool isDismissible = true,
+    bool enableDrag = true,
+    RouteSettings? routeSettings,
+    AnimationController? transitionAnimationController,
+  }) async {
+    currentNavSettings.status.value = PageStatus.leave;
+    var result = await showModalBottomSheet(
+        context: context,
+        builder: builder,
+        backgroundColor: backgroundColor,
+        elevation: elevation,
+        shape: shape,
+        clipBehavior: clipBehavior,
+        constraints: constraints,
+        barrierColor: barrierColor,
+        isScrollControlled: isScrollControlled,
+        useRootNavigator: true,
+        isDismissible: isDismissible,
+        enableDrag: enableDrag,
+        routeSettings: routeSettings,
+        transitionAnimationController: transitionAnimationController);
+    currentNavSettings.status.value = PageStatus.enter;
+    return result;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var pages = _settingsList.map<Page>((settings) => settings.page);
+    return Navigator(
+      key: navigatorKey,
+      onPopPage: (Route<dynamic> route, dynamic result) {
+        if (pop(result)) {
+          return route.didPop(result);
+        } else {
+          return false;
+        }
+      },
+      pages: List.of(pages),
+    );
+  }
+
+  @override
+  Future<bool> popRoute() async {
+    var done = await super.popRoute();
+    if (done) {
+      return done;
+    } else {
+      if (!pop()) {
+        exitCount._tryAdd();
+      }
+      return true;
+    }
+  }
+
+  final _navigatorKey = GlobalKey<NavigatorState>();
+  @override
+  GlobalKey<NavigatorState>? get navigatorKey => _navigatorKey;
+
+  @override
+  Future<void> setNewRoutePath(RouteSettings configuration) async {}
+
+  setInitialPages<T>(List<Page> pages) {
+    for (var page in pages) {
+      _settingsList.add(NavSettings.fromPage(page));
+    }
+  }
+
+  _ensureOne<T>(Page<T> page) {
+    var setttings = NavSettings.fromPage(page);
+    var index = _settingsList.indexOf(setttings);
+
+    if (index > -1) {
+      _settingsList.removeRange(index + 1, _settingsList.length);
+      currentNavSettings.status.value = PageStatus.enter;
+    } else {
+      currentNavSettings.status.value = PageStatus.leave;
+      _settingsList.add(setttings);
+    }
+  }
+
+  Future<T?> push<T>(Page<T> page) {
+    //如果已存在，那么 上面的全弹出。
+    assert(_settingsList.isNotEmpty);
+    _ensureOne<T>(page);
+    notifyListeners();
+    return currentNavSettings.completer.future as Future<T?>;
+  }
+
+  bool pop<T extends Object>([T? result]) {
+    if (_settingsList.length < 2) {
+      return false;
+    }
+    var last = _settingsList.removeLast();
+    notifyListeners();
+    last.completer.complete(result);
+
+    currentNavSettings.status.value = PageStatus.enter;
+
+    return true;
+  }
+
+  Future<T?> replace<T, TO>(Page<T> page, {TO? result}) {
+    assert(_settingsList.isNotEmpty);
+    var last = _settingsList.removeLast();
+    last.completer.complete(result);
+
+    _ensureOne<T>(page);
+    notifyListeners();
+    return currentNavSettings.completer.future as Future<T?>;
+  }
+
+  //--------- private
+
+  final List<NavSettings> _settingsList = [];
+}
+
+enum PageStatus {
+  none,
+  // 被别的页面挡住。
+  leave,
+  // 成为顶层页面。
+  enter
+}
+
+class ExitCount extends ChangeNotifier {
+  int _value = 0;
+  final Duration delay;
+  ExitCount({required this.delay});
+
+  int get value => _value;
+
+  _tryAdd() {
+    _value++;
+    notifyListeners();
+    Timer(delay, () {
+      _value--;
+    });
+  }
+}
+
+class NavSettings<T> {
+  final dynamic page;
+  final Completer<T> completer;
+
+  final status = ValueNotifier<PageStatus>(PageStatus.none);
+  NavSettings({required this.page, required this.completer});
+  factory NavSettings.fromPage(Page<T> page) {
+    return NavSettings<T>(page: page, completer: Completer<T>());
+  }
+  @override
+  int get hashCode => page.child.runtimeType.hashCode;
+
+  @override
+  bool operator ==(Object other) {
+    return other is NavSettings && other.hashCode == hashCode;
+  }
+}
